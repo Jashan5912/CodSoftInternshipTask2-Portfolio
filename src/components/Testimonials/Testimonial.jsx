d88dfeeef79c644feb37681991a92e2efd097218
iamjashan <jashan5912p@gmail.com>
2023-08-30 13:40:02 +0530

    MY Portfolio

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+        "Exceeded expectations with stunning frontend designs that elevated our brand's online presence.",
+    },
+    {
+      img: profilePic2,
+      review:
+        "Innovative problem solver who seamlessly blends frontend magic with solid software foundations.",
+    },
+    {
+      img: profilePic3,
+      review:
+        "Transformed our vision into reality by skillfully harmonizing aesthetics and functionality.",
+    },
+    {
+      img: profilePic4,
+      review:
+        "Reliable partner, delivering top-notch user interfaces and adapting to our evolving needs.",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+      <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+      <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
